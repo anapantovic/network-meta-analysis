9642099afaa8f22c37e8ce458c34e1e1d6c487fe
Ana Pantovic <jelenkovicana5@gmail.com>
2026-02-27 14:26:17 +0100

    updating comments for clarity

--- nma_binary_smoking_cessation.R
+++ nma_binary_smoking_cessation.R
@@ -5,13 +5,14 @@ library(netmeta)
 data("smokingcessation", package = "netmeta")
 str(smokingcessation)
 head(smokingcessation)
-
 #adding the missing column for study ID
 smokingcessation$studlab <- paste0("Study_", 1:nrow(smokingcessation))
-smokingcessation <- smokingcessation[, 
-  c("studlab", setdiff(names(smokingcessation), "studlab"))] #placing the study ID column as first one
 
-#conducting a pairwise meta-analysis on binary data to obtain log odds ratios and corresponding standard errors from each study
+#placing the study ID column as first one
+smokingcessation <- smokingcessation[, 
+  c("studlab", setdiff(names(smokingcessation), "studlab"))] 
+  
+#conducting pairwise meta-analysis to obtain relative treatment effects (log odds ratios) and corresponding standard errors from each study
 pw <- pairwise(
   treat = list(treat1, treat2, treat3),
   event = list(event1, event2, event3),
@@ -22,7 +23,7 @@ pw <- pairwise(
 )
 head (pw)
 
-
+#conducting NMA on pw
 net1 <- netmeta(
   TE,
   seTE,
@@ -35,17 +36,31 @@ net1 <- netmeta(
   reference.group = "A"   
 )
 summary (net1)
+
+#obtaining the graphical presentation of the network
 netgraph(net1,multiarm=TRUE, plastic = TRUE)
+
+#producing the league table
 netleague(net1, digits = 2)
+
+#ranking of treatments
 netrank(net1, small.values = "good")
 rankogram(net1, small.values="good")
+
+#producing a forest plot
 forest(net1)
+
+#evaluating heterogeneity and inconsistenacy
 net1$Q
 net1$df.Q
 net1$tau
 netsplit(net1)
 netheat(net1)
+
+#conducting sensitivity analysis - one out
 netimpact(net1)
+
+#comparing estimates from fixed vs random effects models
 net_fixed <- update(net1, random = FALSE)
 summary(net_fixed)
 summary(net1)
